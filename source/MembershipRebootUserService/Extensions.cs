603c7565aa87f5daedfc5707d124412fed8964e0
Brock Allen <brockallen@gmail.com>
2014-07-22 11:14:22 -0400

    initial code

---
+++ source/MembershipRebootUserService/Extensions.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license
+ */
+
+using System;
+
+namespace Thinktecture.IdentityServer.MembershipReboot
+{
+    static class Extensions
+    {
+        public static Guid ToGuid(this string s)
+        {
+            Guid g;
+            if (!String.IsNullOrWhiteSpace(s) &&
+                Guid.TryParse(s, out g))
+            {
+                return g;
+            }
+            
+            return Guid.Empty;
+        }
+    }
+}
