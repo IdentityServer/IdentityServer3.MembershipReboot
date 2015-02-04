ff0884d0d33e949a6d9581b7ce6e5b0910f13dd0
Brock Allen <brockallen@gmail.com>
2015-02-04 06:10:03 -0800

    use number for updated at claim

--- source/MembershipRebootUserService/MembershipRebootUserService.cs
+++ source/MembershipRebootUserService/MembershipRebootUserService.cs
@@ -67,7 +67,7 @@ namespace Thinktecture.IdentityServer.MembershipReboot
         {
             var claims = new List<Claim>{
                 new Claim(Constants.ClaimTypes.Subject, account.ID.ToString("D")),
-                new Claim(Constants.ClaimTypes.UpdatedAt, account.LastUpdated.ToEpochTime().ToString()),
+                new Claim(Constants.ClaimTypes.UpdatedAt, account.LastUpdated.ToEpochTime().ToString(), ClaimValueTypes.Integer),
                 new Claim("tenant", account.Tenant),
                 new Claim(Constants.ClaimTypes.PreferredUserName, account.Username),
             };
